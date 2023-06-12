81b80dea97d560d4d5d58340620166d4d3b8bf5d
yashweblife <yashweblife20@gmail.com>
2023-06-12 01:40:23 -0500

    format document

--- extension.js
+++ extension.js
@@ -36,13 +36,13 @@ function getWebViewContent(p) {
  * @param {string} name 
  * @returns 
  */
-function stageFile(name=undefined){
+function stageFile(name = undefined) {
 	if (!isValidGitRepo()) {
 		return
 	}
 	const editor = vscode.window.activeTextEditor;
 	if (editor) {
-		const file = (name!== undefined)? name : editor.document.uri.fsPath;
+		const file = (name !== undefined) ? name : editor.document.uri.fsPath;
 		gm.add(file, (err) => {
 			if (err) {
 				vscode.window.showErrorMessage(command_fail + "\n" + err.message)
@@ -55,7 +55,7 @@ function stageFile(name=undefined){
 /**
  * Commit Changes, opens a message box
  */
-function commitChanges(){
+function commitChanges() {
 	if (!isValidGitRepo()) {
 		return;
 	}
@@ -74,7 +74,7 @@ function commitChanges(){
  * Push changes to origin
  * @returns 
  */
-function pushChanges(){
+function pushChanges() {
 	if (!isValidGitRepo()) {
 		return;
 	}
@@ -100,7 +100,7 @@ function pushChanges(){
  * Pull changes from the origin
  * @returns 
  */
-function pullChanges(){
+function pullChanges() {
 	if (!isValidGitRepo()) {
 		return;
 	}
@@ -111,21 +111,21 @@ function pullChanges(){
  * Manage the web-view system
  * @param {any} context 
  */
-function handleViewer(context){
-	const panel = vscode.window.createWebviewPanel("git-more-view", "Git More View", vscode.ViewColumn.One, 
-	{
-		enableScripts:true,
-		// localResourceRoots:[vscode.Uri.file(path.join(context.extensionPath,'webview'))],
-	})
+function handleViewer(context) {
+	const panel = vscode.window.createWebviewPanel("git-more-view", "Git More View", vscode.ViewColumn.One,
+		{
+			enableScripts: true,
+			// localResourceRoots:[vscode.Uri.file(path.join(context.extensionPath,'webview'))],
+		})
 	panel.webview.html = getWebViewContent("./app/index.html")
-	gm.status((error, result)=>{
+	gm.status((error, result) => {
 		panel.webview.postMessage({
-			message:"Hello World",
+			message: "Hello World",
 			stats: result
 		})
 	})
-	panel.webview.onDidReceiveMessage((message)=>{
-		switch(message.command){
+	panel.webview.onDidReceiveMessage((message) => {
+		switch (message.command) {
 			case 'add-to-stage':
 				stageFile(message.name);
 				vscode.window.showInformationMessage("Added: " + message.name);
@@ -136,14 +136,14 @@ function handleViewer(context){
 /**
  * Checkout user to specified branch
  */
-function handleCheckout(){
-	gm.branch().then((val)=>{
-		vscode.window.showQuickPick(val.all).then((val)=>{
-			if(val){
-				gm.checkout(val, (error)=>{
-					if(error){
+function handleCheckout() {
+	gm.branch().then((val) => {
+		vscode.window.showQuickPick(val.all).then((val) => {
+			if (val) {
+				gm.checkout(val, (error) => {
+					if (error) {
 						vscode.window.showErrorMessage(error.message)
-					}else{
+					} else {
 						vscode.window.showInformationMessage("Checked Out To: " + val)
 					}
 				})
