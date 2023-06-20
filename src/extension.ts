48f1b797eda83cd0fe1686a9764ab0f7d8a6f6e7
yashweblife <yashweblife20@gmail.com>
2023-06-20 02:45:30 -0500

    import GitMore

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,12 @@
 import * as vscode from 'vscode';
-
+import { GitMore } from './GitMore';
 export function activate(context: vscode.ExtensionContext) {
 
+	const gm = new GitMore()
 	console.log('Congratulations, your extension "git-more" is now active!');
 
-	let disposable = vscode.commands.registerCommand('git-more.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from !');
+	let disposable = vscode.commands.registerCommand('git-more.stage', () => {
+		gm.stageCurrentFile()
 	});
 
 	context.subscriptions.push(disposable);
